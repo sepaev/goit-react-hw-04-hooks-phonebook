705e2407b9c82c25f1fc1f883acec5241d312fda
sepaev <sepaev@gmail.com>
2021-09-17 01:16:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import { Component, Fragment } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { Section } from "./components/Section";
+import { Component, Fragment, useState, use } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Section from './components/Section';
 
-import { Notify } from "notiflix";
-Notify.init({ position: "center-top" });
+import { Notify } from 'notiflix';
+Notify.init({ position: 'center-top' });
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: "",
-  };
-  doAddContact = (name, number) => {
-    Notify.success("Well Done! Added " + name);
-    this.setState(({ contacts, filter }) => {
-      const newArr = [...contacts, { id: uuidv4(), name, number }];
-      return { contacts: newArr, filter };
-    });
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const makeSearch = e => {
+    const searchQuery = e.target.value.toLowerCase();
+    setFilter(searchQuery);
   };
 
-  makeSearch = (e) => {
-    const searchQuery = e.target.value.toLocaleLowerCase();
-    this.setState({ filter: searchQuery });
-  };
-
-  doDeleteContact = (id) => {
-    const { contacts, filter } = this.state;
+  const doDeleteContact = id => {
     const newArr = [];
     let reportName;
     if (!id) return;
 
-    contacts.forEach((contact) => {
+    contacts.forEach(contact => {
       if (contact.id !== id) {
         newArr.push(contact);
       } else {
@@ -38,50 +28,49 @@ class App extends Component {
     });
 
     if (newArr.length === contacts.length) {
-      Notify.failure("Oh, no!");
+      Notify.failure('Oh, no! Nothing was deleted.');
       return;
     }
-
-    this.setState(() => {
-      return { contacts: newArr, filter };
-    });
-
+    setContacts(newArr);
     Notify.info(`Contact ${reportName} was removed successfully`);
   };
+
+  const doAddContact = (name, number) => {
+    setContacts(prevState => [...prevState, { id: uuidv4(), name, number }]);
+    Notify.success('Well Done! Added ' + name);
+  };
+
+  return (
+    <Fragment>
+      <Section title='Phonebook' component='Form' data={{ contacts, filter }} doAddContact={doAddContact} />
+      <hr />
+      <Section
+        title='Contacts'
+        component='Contacts'
+        data={{ contacts, filter }}
+        searchFunc={makeSearch}
+        deleteFunc={doDeleteContact}
+      />
+    </Fragment>
+  );
+}
+
+class AppOld extends Component {
   componentDidMount() {
-    const storage = JSON.parse(localStorage.getItem("contacts"));
+    const storage = JSON.parse(localStorage.getItem('contacts'));
     if (storage) {
       this.setState({ contacts: storage });
     } else {
-      this.setState({ contscts: [], filter: "" });
+      this.setState({ contscts: [], filter: '' });
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
-  render() {
-    return (
-      <Fragment>
-        <Section
-          title="Phonebook"
-          component="Form"
-          data={this.state}
-          doAddContact={this.doAddContact}
-        />
-        <hr />
-        <Section
-          title="Contacts"
-          component="Contacts"
-          data={this.state}
-          searchFunc={this.makeSearch}
-          deleteFunc={this.doDeleteContact}
-        />
-      </Fragment>
-    );
-  }
+  render() {}
 }
 
 export default App;
